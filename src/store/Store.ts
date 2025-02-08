6a1f4a506e9b66ec98604d85c635642cfc05404d
MinuraWije <minurawije@gmail.com>
2025-02-08 12:15:10 +0530

    updated store reducers

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,10 +1,18 @@
-import CustomerSlice from "../reducers/CustomerSlice";
 import {configureStore} from "@reduxjs/toolkit";
 import EquipmentSlice from "../reducers/EquipmentSlice.ts";
+import VehicleSlice from "../reducers/VehicleSlice.ts";
+import StaffSlice from "../reducers/StaffSlice.ts";
+import CropSlice from "../reducers/CropSlice.ts";
+import LogSlice from "../reducers/LogSlice.ts";
+import FieldSlice from "../reducers/FieldSlice.ts";
 
 export const store =configureStore({
     reducer:{
         equipments: EquipmentSlice,
-        customer: CustomerSlice
+        vehicles: VehicleSlice,
+        staff: StaffSlice,
+        crop: CropSlice,
+        log: LogSlice,
+        field: FieldSlice,
     }
 })
